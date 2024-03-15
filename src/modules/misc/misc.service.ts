a6352f8bb80086c502a8dade22a899333b8f51e7
1Domz <dominiquesimbillo03@gmail.com>
2024-03-15 17:34:12 +0800

    added create survey

--- src/modules/misc/misc.service.ts
+++ src/modules/misc/misc.service.ts
@@ -1,6 +1,8 @@
 import prisma from '@/lib/prisma';
-import { CreateFaqDto, addFeedbackDto } from '@/dto/misc.dto';
-import { Rating } from '@prisma/client';
+import { CreateFaqDto, addFeedbackDto, SurveyDto } from '@/dto/misc.dto';
+import { Rating, Marketing } from '@prisma/client';
+import { JwtPayload } from '@/types/common.type';
+import { HttpUnAuthorizedError } from '@/lib/errors';
 export default class MiscService {
   public async getFaq() {
     return await prisma.faq.findMany();
@@ -23,5 +25,18 @@ export default class MiscService {
         rating: data.rating as Rating,
       },
     });
+  }
+
+  public async SurveyData(data: SurveyDto) {
+    try {
+      return await prisma.survey.create({
+        data: {
+          marketing: data.marketing,
+          userId: data.userId,
+        },
+      });
+    } catch (error) {
+      throw new HttpUnAuthorizedError('Error');
+    }
   }
 }
