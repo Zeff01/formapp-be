306d1d8abd0e365e88370e920531d20346a67ab4
1Domz <dominiquesimbillo03@gmail.com>
2024-03-26 21:51:16 +0800

    Dashboard collection Added - total onlinePayment - total cashPayment - grandTotal - transaction details

--- src/modules/sessions/xendit.service.ts
+++ src/modules/sessions/xendit.service.ts
@@ -5,12 +5,14 @@ import {
   IXenditPayoutDto,
   XCreateSubscriptionPlan,
 } from '@/dto/xendit.dto';
+import prisma from '@/lib/prisma';
 
 interface Transaction {
   amount: number;
 }
 export default class XenditService {
   private readonly API_GATEWAY_URL = 'https://api.xendit.co';
+  private readonly API_DEV_URL = 'http://localhost:4000/api/v1/development';
   private readonly CHANNEL_CODE = 'PH_GCASH';
   private readonly CURRENCY = 'PHP';
   private readonly TYPE = 'INDIVIDUAL';
@@ -185,7 +187,54 @@ export default class XenditService {
         const res = (onlinePayment += data.amount);
         return res;
       });
-      return { result, onlinePayment };
+
+      let cashPayment = 0;
+
+      const getRates = await axios.get(
+        this.API_DEV_URL + `/sessions/rates?from=${from}&to=${to}`
+      );
+      const RatesData = getRates.data;
+      RatesData.data.forEach((data) => (cashPayment += data.cashRate));
+
+      const grandTotal = onlinePayment + cashPayment;
+
+      const bankDetailsResponse = await axios.get(
+        this.API_DEV_URL + `/misc/bank`
+      );
+
+      const bankDetails = bankDetailsResponse.data;
+
+      const transactionDetails = result.data.map((data) => {
+        const bankDetail = bankDetails.data.find((detail) => {
+          return detail.channelCode === data.channel_code;
+        });
+
+        if (bankDetail) {
+          const getDate = new Date(data.created);
+          const month = (getDate.getMonth() + 1).toString().padStart(2, '0');
+          const day = getDate.getDate().toString().padStart(2, '0');
+          const year = getDate.getFullYear().toString().padStart(2, '0');
+          const date = month + '/' + day + '/' + year;
+          let hours: string = getDate.getHours().toString();
+          const ampm = hours >= '12' ? 'PM' : 'AM';
+          hours = (parseInt(hours) % 12 || 12).toString().padStart(2, '0'); 
+          const minutes = getDate.getMinutes().toString().padStart(2, '0'); 
+          const time = hours + ':' + minutes + ' ' + ampm;
+
+          const bankName = bankDetail.bankName;
+          const amount = data.amount;
+          return { bankName, date, time, amount };
+        } else {
+          return data;
+        }
+      });
+
+      return {
+        onlinePayment,
+        cashPayment,
+        grandTotal,
+        transactionDetails,
+      };
     } catch (error) {
       console.error('HTTP Request Error:', error);
       throw error;
