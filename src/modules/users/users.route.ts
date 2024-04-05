badb8dc26e059bdcc9a9f97b8dd80a8d70badc55
1Domz <dominiquesimbillo03@gmail.com>
2024-04-05 09:53:10 +0800

    docs: added swagger for createUser

--- src/modules/users/users.route.ts
+++ src/modules/users/users.route.ts
@@ -16,39 +16,40 @@ const users: Router = Router();
 const controller = new Controller();
 
 /**
- * User
- * @typedef {object} User
+ * Create User Body
+ * @typedef {object} CreateUserBody
+ * @property {string} email.required - email of user
+ * @property {string} password.required - password of user
+ * @property {string} firstname.required - firstname of user
+ * @property {string} lastname.required - lastname of user
+ * @property {string} phone.required - phone number
+ * @property {string} dateofbirth.required - birth date
+ * @property {string} address.required - address of user
+ * @property {string} profilePic.required - Profile Pic URL
+ */
+
+/**
+ * Create User
+ * @typedef {object} CreateUser
  * @property {string} email - email of user
  * @property {string} password - password of user
- * @property {string} firstName - firstname of user
- * @property {string} lastName - lastname of user
+ * @property {string} firstname - firstname of user
+ * @property {string} lastname - lastname of user
+ * @property {string} phone - phone number
+ * @property {string} dateofbirth - birth date
+ * @property {string} address - address of user
  * @property {string} profilePic - Profile Pic URL
- * @property {string} phone - Phone Number
- * @property {string} type - User Type | ADMIN, FOUNDER,USER
  */
 
 /**
  * POST /users
- * @typedef {object} CreateUserDto
- * @summary Create Player
+ * @summary Create user
  * @tags users
- * @property {string} firstName.required - User Firstname
- * @property {string} lastName.required - User LastName
- * @property {string} phone.required - Phone Number
- * @property {string} dateofbirth.required - Date of Birth (1998-01-01 00:00:00Z)
- * @property {string} address.required - Date of Birth (1998-01-01 00:00:00Z)
- * @property {string} email.required - User Email
- * @property {string} gender.required -  Gender
- * @property {string} password.required - User Password
- * @property {string} profilePic - Profile pic URL
- * @param {CreateUserDto} request.body.required
- * @return {User} 201 - Player Successfully Created
+ * @param {CreateUserBody} request.body.required
+ * @return {CreateUser} 201 - Created User
  */
 
-users
-  .route('')
-  .post(RequestValidator.validate(CreateUserDto), controller.createUser)
-  .get(verifyAuthToken, controller.getFounderInfo);
+users.post('', RequestValidator.validate(CreateUserDto), controller.createUser);
 
 /**
  * POST /users/staff
@@ -192,11 +193,11 @@ users.post(
   controller.createClub
 );
 
-//TODO: ARRANGE USER / CLUB 
+//TODO: ARRANGE USER / CLUB
 
 //TODO: Document getSubscription
 
-users.get('/subscription', controller.getSubscriptionRate)
+users.get('/subscription', controller.getSubscriptionRate);
 
 /**
  * PATCH /users/join
@@ -214,7 +215,6 @@ users.patch('/team/join', verifyAuthToken, controller.joinTeamPerSubSession);
 users.get('/player', verifyAuthToken, controller.getPlayerById);
 
 users.get('/player/find', verifyAdmintAuthToken, controller.getPlayerByName);
-
 
 users.post(
   '/portal/customer',
