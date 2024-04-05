404334d62f1032c443c6bd4f414c6eb0b7a07280
1Domz <dominiquesimbillo03@gmail.com>
2024-04-06 07:41:02 +0800

    docs: - added test for creteFounder and staff - fix other test that failing

---
+++ tests/modules/User/createFounder/createFounder.test.ts
@@ -0,0 +1,42 @@
+import prisma from '../../../../src/lib/prisma';
+import UserService from '../../../../src/modules/users/users.service';
+import { UserData } from './createFounder.data';
+jest.mock('../../../../src/lib/prisma', () => ({
+  __esModule: true,
+  default: {
+    users: {
+      create: jest.fn(),
+      findUnique: jest.fn(),
+    },
+  },
+}));
+
+describe('Create User', () => {
+  let userService: UserService;
+  beforeAll(() => {
+    userService = new UserService();
+  });
+  beforeEach(() => jest.clearAllMocks());
+
+  it('should create a new user', async () => {
+    (prisma.users.create as jest.Mock).mockImplementation(() => {
+      return Promise.resolve(UserData);
+    });
+
+    const result = await userService.createFounder(UserData);
+
+    expect(result).toEqual(UserData);
+  });
+
+  it('should error if user already existing email', async () => {
+    (prisma.users.findUnique as jest.Mock).mockResolvedValue({
+      email: UserData.email,
+    });
+
+    try {
+      await userService.createUser(UserData);
+    } catch (error) {
+      expect(error.message).toEqual('Email already exist');
+    }
+  });
+});
