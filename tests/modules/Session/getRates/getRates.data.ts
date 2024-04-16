5689f7dfc33e1c3ce83e6c2473ba10bbd6b04f8a
awyn <alwin.puche16@gmail.com>
2024-04-16 10:42:39 +0900

    add test for get rates (WIP)

---
+++ tests/modules/Session/getRates/getRates.data.ts
@@ -0,0 +1,17 @@
+/* eslint-disable */
+
+const data = {
+    message: 'Rates List',
+    data: [{
+        id: "49821a07-4661-4208-b234-93416424f7b5";
+        packageName: string;
+        cashRate: number;
+        onlineRate: number;
+        sessionCount: number;
+        description: string;
+        createdAt: Date | null;
+        updatedAt: Date | null;
+        status: $Enums.RecordStatus;
+        subSessionId: "c3ee861e-9ef4-4099-b663-143afffae85c"}
+    ]
+};
