bc9a8547eb8daa40e94ec1bde274d2c019a2ac1c
awyn <alwin.puche16@gmail.com>
2024-04-17 01:47:42 +0900

    add get rates test (WIP)

--- tests/modules/Session/getRates/getRates.data.ts
+++ tests/modules/Session/getRates/getRates.data.ts
@@ -1,17 +1,85 @@
 /* eslint-disable */
 
-const data = {
-    message: 'Rates List',
-    data: [{
-        id: "49821a07-4661-4208-b234-93416424f7b5";
-        packageName: string;
-        cashRate: number;
-        onlineRate: number;
-        sessionCount: number;
-        description: string;
-        createdAt: Date | null;
-        updatedAt: Date | null;
-        status: $Enums.RecordStatus;
-        subSessionId: "c3ee861e-9ef4-4099-b663-143afffae85c"}
-    ]
+export const data = {
+  message: 'Rate List',
+  data: [
+    {
+      id: '123',
+      packageName: '1 Session Tourney',
+      cashRate: 250,
+      onlineRate: 300,
+      sessionCount: 1,
+      description:
+        'lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum',
+      createdAt: '2024-04-15T00:54:00.100Z',
+      updatedAt: '2024-04-15T00:54:00.100Z',
+      status: 'ACTIVE',
+      subSessionId: 'f375358a-87b7-42ba-abd7-867681bf6da3',
+    },
+    {
+      id: '8078a989-9104-4ff5-943b-ad416fb260de',
+      packageName: '2 Session Tourney',
+      cashRate: 500,
+      onlineRate: 550,
+      sessionCount: 2,
+      description:
+        'lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum',
+      createdAt: '2024-04-15T00:54:00.100Z',
+      updatedAt: '2024-04-15T00:54:00.100Z',
+      status: 'ACTIVE',
+      subSessionId: 'f375358a-87b7-42ba-abd7-867681bf6da3',
+    },
+    {
+      id: '00d3d3c6-e792-499a-92cc-40569488eb54',
+      packageName: '3 Session Tourney',
+      cashRate: 700,
+      onlineRate: 750,
+      sessionCount: 5,
+      description:
+        'lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum',
+      createdAt: '2024-04-15T00:54:00.100Z',
+      updatedAt: '2024-04-15T00:54:00.100Z',
+      status: 'ACTIVE',
+      subSessionId: 'f375358a-87b7-42ba-abd7-867681bf6da3',
+    },
+    {
+      id: '0109348d-9084-43d0-91db-ff0cfff3586d',
+      packageName: '4 Session Tourney',
+      cashRate: 250,
+      onlineRate: 300,
+      sessionCount: 1,
+      description:
+        'lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum',
+      createdAt: '2024-04-15T00:54:40.789Z',
+      updatedAt: '2024-04-15T00:54:40.789Z',
+      status: 'ACTIVE',
+      subSessionId: 'e2cd2989-4af0-40b8-beda-372e1a6cfaac',
+    },
+    {
+      id: 'c4aafe7e-6b6d-4183-9e39-f47939831b1b',
+      packageName: '5 Session Tourney',
+      cashRate: 500,
+      onlineRate: 550,
+      sessionCount: 2,
+      description:
+        'lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum',
+      createdAt: '2024-04-15T00:54:40.789Z',
+      updatedAt: '2024-04-15T00:54:40.789Z',
+      status: 'ACTIVE',
+      subSessionId: 'e2cd2989-4af0-40b8-beda-372e1a6cfaac',
+    },
+    {
+      id: 'aa0556a7-13e3-4a3e-8c5b-4dac30337981',
+      packageName: '6 Session Tourney',
+      cashRate: 700,
+      onlineRate: 750,
+      sessionCount: 5,
+      description:
+        'lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum',
+      createdAt: '2024-04-15T00:54:40.789Z',
+      updatedAt: '2024-04-15T00:54:40.789Z',
+      status: 'ACTIVE',
+      subSessionId: 'e2cd2989-4af0-40b8-beda-372e1a6cfaac',
+    },
+  ],
 };
--- tests/modules/Session/getRates/getRates.test.ts
+++ tests/modules/Session/getRates/getRates.test.ts
@@ -1,20 +1,56 @@
 /* eslint-disable */
 import SessionsService from '../../../../src/modules/sessions/sessions.service';
 import prisma from '../../../../src/lib/prisma';
+import { data } from './getRates.data';
+import { rates } from '@prisma/client';
+import dayjs from 'dayjs';
 
 jest.mock('../../../../src/lib/prisma', () => ({
   __esModule: true,
   default: {
-    sessions: {
+    rates: {
       findMany: jest.fn(),
     },
   },
 }));
 
 describe('Session Rates', () => {
-  let sesionService: SessionsService;
+  let sessionService: SessionsService;
 
   beforeAll(() => {
-    const mockFindMany = jest.fn().mockResolvedValue(data.data);
+    sessionService = new SessionsService();
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should return all rates', async () => {
+    (prisma.rates.findMany as jest.Mock).mockResolvedValue(data.data);
+    const rates = await sessionService.getRates();
+    console.log(rates);
+    expect(rates).toBe(data.data);
+  });
+
+  it('should return based on the date ', async () => {
+    const fromDate = new Date('2024-04-13');
+    const toDate = new Date('2024-05-1');
+    const fromUnix = dayjs(fromDate).unix();
+    const toUnix = dayjs(toDate).unix();
+
+    (prisma.rates.findMany as jest.Mock).mockResolvedValue(
+      data.data.filter((rate) => {
+        const createdAt = dayjs(rate.createdAt).unix();
+        return createdAt >= fromUnix && createdAt <= toUnix;
+      })
+    );
+
+    const rates: rates[] = await sessionService.getRates(fromDate, toDate);
+    console.log(rates.length);
+    rates.forEach((rate) => {
+      const createdAt = dayjs(rate.createdAt).unix();
+      const isValid = createdAt >= fromUnix && createdAt <= toUnix;
+      expect(isValid).toEqual(true);
+    });
   });
 });
