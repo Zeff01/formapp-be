8258bf37335b92553dba2be850ec62b7700f0df1
awyn <alwin.puche16@gmail.com>
2024-04-18 13:40:38 +0800

    test: add getGamePerSubId test

---
+++ tests/modules/Session/getGamePerSubId/getGamePerSubId.data.ts
@@ -0,0 +1,49 @@
+/* eslint-disable */
+  export const data = {
+    "message": "Game Screen Details",
+    "data": { 
+        "id": "7dcad6d1-1670-4eba-970d-e65e76621f5a",
+        "sessionType": "TOURNAMENT",
+        "coach": "Messi",
+        "noofTeams": 2,
+        "maxPlayers": 20,
+        "maxperTeam": 10,
+        "createdAt": "2024-04-02T02:49:52.750Z",
+        "updatedAt": "2024-04-02T02:49:52.750Z",
+        "sessionId": "49821a07-4661-4208-b234-93416424f7b5",
+        "status": "ACTIVE",
+        "createdBy": "1ca37d58-4eb0-4f65-a144-298f2fb4a8ff",
+        "sessions": {
+            "id": "49821a07-4661-4208-b234-93416424f7b5",
+            "name": "Session 2",
+            "location": "Makati",
+            "sessionDate": "02/22/2024",
+            "sessionTime": "08:00"
+        },
+        "teams": [
+            {
+                "id": "85a66eb0-fa7f-4b5d-a3db-83062a6ce718",
+                "teamName": "Team A",
+                "color": "Red",
+                "subSessionId": "7dcad6d1-1670-4eba-970d-e65e76621f5a",
+                "users": [],
+                "_count": {
+                    "users": 0
+                }
+            },
+            {
+                "id": "1ab4604e-4dad-4797-81b3-c5f7b60f4557",
+                "teamName": "Team B",
+                "color": "Blue",
+                "subSessionId": "7dcad6d1-1670-4eba-970d-e65e76621f5a",
+                "users": [],
+                "_count": {
+                    "users": 0
+                }
+            }
+        ],
+        "users": []
+    }
+}  
+ 
+ 
